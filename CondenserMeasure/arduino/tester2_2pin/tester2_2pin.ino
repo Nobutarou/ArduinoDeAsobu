63aacf757b88b03bbcde0841a8f98ea507b7902a
Nobutarou <nobutarounosuke@gmail.com>
2023-12-08 19:37:47 +0900

    fix #11

--- CondenserMeasure/arduino/tester2_2pin/tester2_2pin.ino
+++ CondenserMeasure/arduino/tester2_2pin/tester2_2pin.ino
@@ -1,6 +1,6 @@
-#define _R 10000.0; // 実測値
-
-#include <Arduino.h>
+// シリアル通信について TinkerCad と実物で相違がある。
+// Linux の改行が <LF><CR> だかで、文字+Enter という送信になってるのかも
+// まあ Windows 使わないからいいや
 
 const char PULSE_PIN = 2;
 const char ANALOG_READ_PIN = A5;
@@ -8,7 +8,7 @@ const char ANALOG_READ_PIN = A5;
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("setup");
+  Serial.setTimeout(2^32-1); //no timeout
   pinMode( PULSE_PIN , OUTPUT);
   digitalWrite(PULSE_PIN, LOW);
 
@@ -35,17 +35,52 @@ unsigned long charge() {
 }
 
 void loop() {
-  discharge();
+  double _R;
+  _R=r_setup();
 
-  unsigned long time_start = charge();
+  // TinkerCad では文字を送る必要があったが、実物では Enter で反応する
+  Serial.println("Enter to reset registor");
 
-  while ( analogRead(ANALOG_READ_PIN) < 647)
-  {
-  }; //1023*64.2=646.5。
-
-  double T = micros() - time_start; // T: 時定数
-  double c=T/_R; // [uF]
+  // TinkerCad では不要だったけど、これがないと Serial.available()>0 になってしまう。
+  Serial.end();
+  Serial.begin(9600);
   
-  Serial.println(c,16);
-  digitalWrite(PULSE_PIN, LOW);
+  while(Serial.available()==0){
+    discharge();
+
+    unsigned long time_start = charge();
+
+    while ( analogRead(ANALOG_READ_PIN) < 647){};//1023*64.2=646.5。
+
+    double T = micros() - time_start; // T: 時定数
+    double c=T/_R; // [uF]
+  
+    Serial.println(c,16);
+    digitalWrite(PULSE_PIN, LOW);
+  }
+  Serial.end();
+  Serial.begin(9600);
+}
+
+double r_setup()
+{
+  double registor;
+  double x; // general purpose
+  short i; //
+  char a; // general purpose
+  
+  Serial.println("Input the registor [Ohm]");
+  Serial.println("Scientific notation only (ex, 1.23e-4)");
+
+  while (Serial.available() == 0) {};
+  x=Serial.parseFloat();
+  a=Serial.read();
+  i=Serial.parseInt();
+  
+  registor=x*pow(10.0,i);
+  
+  Serial.print("Registor: ");
+  Serial.print(registor,16);
+  Serial.println(" Ohm");
+  return registor;
 }
